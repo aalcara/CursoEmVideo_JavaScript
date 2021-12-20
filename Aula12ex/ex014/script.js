545740d01d6f01603451af027469c857c91a6212
Anderson <ander_alvarenga@hotmail.com>
2021-12-20 20:09:20 -0300

    exercicios

---
+++ Aula12ex/ex014/script.js
@@ -0,0 +1,18 @@
+function carregar () {
+  var msg = window.document.getElementById("msg")
+  var img = window.document.getElementById("imagem")
+  var data = new Date()
+  var hora = data.getHours()
+  // var hora =22
+  msg.innerHTML = `Agora são ${hora} horas.`
+  if (hora >= 0 && hora < 12) {
+    img.src = "morning.png"
+    document.body.style.background = '#bed2e5'
+  } else if (hora >= 12 && hora <= 18) {
+    img.src = 'afternoon.png'
+    document.body.style.background = '#ec855b'
+  } else {
+    img.src = 'night.png'
+    document.body.style.background = '#01386b'
+  }
+}
