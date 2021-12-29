4432bac212a2a728f8b84e7be58004d81d10199a
Anderson <ander_alvarenga@hotmail.com>
2021-12-29 09:26:11 -0300

    Exercicios Aula 14

--- Aula14ex/ex016/script.js
+++ Aula14ex/ex016/script.js
@@ -1,13 +1,51 @@
 var button = window.document.querySelector("input#verifybutton")
 
-button.addEventListener('click', verify)
+button.addEventListener('click', calcular)
 button.addEventListener('mouseenter', change_color)
 button.addEventListener('mouseout', change_back_color)
-
-
 function change_color() {
   button.style.background = "rgb(101, 116, 184)"
 }
 function change_back_color() {
   button.style.background = "rgb(17, 42, 153)"
+}
+
+
+function calcular() {
+  var txti = window.document.getElementById("inicio").value
+  var txtf = window.document.getElementById("fim").value
+  var txtp = window.document.getElementById("passo").value
+  var inicio = Number(txti)
+  var fim = Number(txtf)
+  var passo = Number(txtp)
+  var result = window.document.getElementById("result")
+  if (txti.length == 0 || txtf.length == 0 || txtp.length == 0 ) {
+    window.alert("[ERRO] Faltam dados!")
+    result.innerHTML = "<p>Impossível Calcular!</p>"
+  } else {
+    if (passo <= 0) {
+      window.alert("Passo inválido. Considerando passo = 1")
+      passo = 1
+    }
+    var valor = inicio
+    result.innerHTML = "<p>Calculando:</p>"
+
+    var calc = window.document.createElement("div")
+    calc.setAttribute('id', 'calc')
+    if (valor <= fim) {
+      while (valor <= fim) {
+        calc.innerHTML += `${valor} `
+        calc.innerHTML += "👉 "
+        valor += passo
+      }
+    } else {
+      while (valor >= fim) {
+        calc.innerHTML += `${valor} `
+        calc.innerHTML += "👉 "
+        valor -= passo
+      }
+    }
+    calc.innerHTML += "🏁"
+    result.appendChild(calc)
+  }
 }
--- Aula14ex/ex017/script.js
+++ Aula14ex/ex017/script.js
@@ -1,1 +1,27 @@
-window.alert("Olá!")
+var button = window.document.querySelector("input#button")
+
+button.addEventListener('click', calcular)
+button.addEventListener('mouseenter', change_color)
+button.addEventListener('mouseout', change_back_color)
+function change_color() {
+  button.style.background = "rgb(17, 42, 153)"
+}
+function change_back_color() {
+  button.style.background = "rgb(101, 116, 184)"
+}
+
+function calcular () {
+  var element_number = window.document.getElementById("number").value
+  if (element_number == "") {
+    window.alert("Insira um valor!")
+    console.log("Vazio")
+  } else {
+    var caixa = window.document.getElementById("caixa")
+    var nbr = Number(element_number)
+    caixa.innerHTML = ""
+    for (var c = 1; c <= 10; c++) {
+      caixa.innerHTML += `<option>${nbr} x ${c} = ${nbr*c}`
+    }
+  }
+  console.log(element_number)
+}
